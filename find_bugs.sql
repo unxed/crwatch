d8a866c241b7f15f4bdc8ab0baa0499d89ebeb4c
Ivan Sorokin <unxed@mail.ru>
2025-08-26 00:18:12 +0200

    get all counters in one query

--- find_bugs.sql
+++ find_bugs.sql
@@ -10,3 +10,11 @@ SELECT COUNT(*) FROM `procurement_locations` WHERE LENGTH(`address`) < 15 OR LEN
 SELECT COUNT(*) FROM `procurement_locations` WHERE LENGTH(`address`) < 15;
 SELECT COUNT(*) FROM `procurement_locations` WHERE LENGTH(`address`) > 200;
 
+
+SELECT
+    COUNT(CASE WHEN LENGTH(address) < 15 THEN 1 END) AS short_address_count,
+    COUNT(CASE WHEN LENGTH(address) > 200 THEN 1 END) AS long_address_count,
+    COUNT(CASE WHEN LENGTH(address) < 15 OR LENGTH(address) > 200 THEN 1 END) AS total_sum
+FROM
+    procurement_locations;
+

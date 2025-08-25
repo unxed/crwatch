6a35d77986b4c35eff813f80c28c00b92db0217e
Ivan Sorokin <unxed@mail.ru>
2025-08-25 15:38:10 +0200

    sql queries helping to find bugs in addresses parsing

---
+++ find_bugs.sql
@@ -0,0 +1,4 @@
+
+SELECT *, LENGTH(`address`) AS `ln` FROM `procurement_locations` ORDER BY `ln` DESC LIMIT 0, 50
+
+SELECT *, LENGTH(`address`) AS `ln` FROM `procurement_locations` ORDER BY `ln` LIMIT 0, 50

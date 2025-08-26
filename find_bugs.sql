51c95e4a98fcda2e8d7346580e781246f9a753e5
Ivan Sorokin <unxed@mail.ru>
2025-08-26 02:13:06 +0200

    cosmetic

--- find_bugs.sql
+++ find_bugs.sql
@@ -1,8 +1,8 @@
 
-SELECT *, LENGTH(`address`) AS `ln` FROM `procurement_locations` ORDER BY `ln` DESC LIMIT 0, 50
 
 SELECT *, LENGTH(`address`) AS `ln` FROM `procurement_locations` ORDER BY `ln` LIMIT 0, 50
 
+SELECT *, LENGTH(`address`) AS `ln` FROM `procurement_locations` ORDER BY `ln` DESC LIMIT 0, 50
 
 
 SELECT COUNT(*) FROM `procurement_locations` WHERE LENGTH(`address`) < 15 OR LENGTH(`address`) > 200;
